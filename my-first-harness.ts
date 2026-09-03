8c5f39d2a21765916e33b241e51f3ce54c2a03c0
Choe Chiwon <choechiwon@chiwonchoi-macos.local>
2026-09-03 16:41:50 +0900

    2. while true 로 '사용자 입력 - LLM응답'루프 만들기.

--- my-first-harness.ts
+++ my-first-harness.ts
@@ -1,26 +1,36 @@
 import 'dotenv/config'
+import { createInterface } from 'node:readline/promises'
 
 const token = process.env.AIPROXY_TOKEN
 
 const input = process.argv[2]
 
-const response = await fetch(
-  'https://aiproxy-api.backoffice.bagelgames.com/api/v1/chat/openai',
-  {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+const terminal = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+while(true) {
+  const input = await terminal.question('> ')
+
+  const response = await fetch(
+    'https://aiproxy-api.backoffice.bagelgames.com/api/v1/chat/openai',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        messages: [{ role: 'user', content: input }],
+      }),
     },
-    body: JSON.stringify({
-      messages: [{ role: 'user', content: input }],
-    }),
-  },
-)
+  )
 
-// console.log(result.content)
-// console.log(response)
+  // console.log(result.content)
+  // console.log(response)
 
 
-const result = await response.json()
-console.log(result.content)
+  const result = await response.json()
+  console.log(result.content)
+}
