98f442b36fc5247552f74d08067ead62953e6719
Choe Chiwon <choechiwon@chiwonchoi-macos.local>
2026-09-03 16:33:34 +0900

    step1. 실행 시 인자를 유저 입력으로 ai proxy 사내 api 활용해 fetch함수로 http호출해서 LLM 응답 받기

---
+++ my-first-harness.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+
+const token = process.env.AIPROXY_TOKEN
+
+const input = process.argv[2]
+
+const response = await fetch(
+  'https://aiproxy-api.backoffice.bagelgames.com/api/v1/chat/openai',
+  {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      messages: [{ role: 'user', content: input }],
+    }),
+  },
+)
+
+// console.log(result.content)
+// console.log(response)
+
+
+const result = await response.json()
+console.log(result.content)
