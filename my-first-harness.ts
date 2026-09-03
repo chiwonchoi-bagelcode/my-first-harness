dc47d3ab592b616cfe6e92d4ae554e1a48850f73
Choe Chiwon <choechiwon@chiwonchoi-macos.local>
2026-09-03 18:51:05 +0900

    4-2 툴 확장 가능하게, 이제 종료를 스스로 판단함(시스템 프롬프트 수정)

--- my-first-harness.ts
+++ my-first-harness.ts
@@ -14,6 +14,42 @@ function getCurrentTime() {
   return new Date().toISOString();
 }
 
+async function getOtherLLMsOpinion(ask: string) {
+  const response = await fetch(
+    "https://aiproxy-api.backoffice.bagelgames.com/api/v1/chat/openai",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "user",
+            content: ask,
+          },
+        ],
+      }),
+    },
+  );
+
+  const result = await response.json();
+
+  return result.content;
+}
+
+let counter = 0;
+function counterUP() {
+  counter++;
+
+  return "successfully increased counter";
+}
+
+function getCounterVal() {
+  return counter;
+}
+
 async function step() {
   console.log(messages);
 
@@ -50,15 +86,34 @@ async function turn(input: string) {
   while (true) {
     const output = await step();
 
-    if (output !== "CALL getCurrentTime") {
+    if (
+      output !== "CALL getCurrentTime" &&
+      output !== "CALL getOtherLLMsOpinion" &&
+      output !== "CALL counterUP" &&
+      output !== "CALL getCounterVal"
+    ) {
       return output;
     }
 
-    const toolResult = getCurrentTime();
+    const toolResult = await (async () => {
+      switch (output) {
+        case "CALL getOtherLLMsOpinion":
+          return await getOtherLLMsOpinion(output);
+
+        case "CALL getCurrentTime":
+          return getCurrentTime();
+
+        case "CALL counterUP":
+          return counterUP();
+
+        case "CALL getCounterVal":
+          return getCounterVal();
+      }
+    })();
 
     messages.push({
       role: "user",
-      content: `TOOL RESULT getCurrentTime: ${toolResult}`,
+      content: `TOOL RESULT : ${toolResult}`,
     });
   }
 }
@@ -66,8 +121,9 @@ async function turn(input: string) {
 const messages = [
   {
     role: "system",
-    content:
-      'You are a smart secretary of your master. answer based on given conversation so far. Btw whenever the master asks you for the current time, just answer exactely "CALL getCurrentTime" without any changing',
+    content: `너는 마스터를 돕는 비서다. 마스터의 요구를 만족하라. 
+      - 사용할 수 있는 도구: getCurrentTime, getOtherLLMsOpinion, counterUP, getCounterVal
+      - 도구 사용법: 어떠한 수정이나 다른 출력도 없이 오직 'CALL {도구이름}'으로 답하라. 예를 들면, 'CALL getCurrentTime'`,
   },
 ];
 
