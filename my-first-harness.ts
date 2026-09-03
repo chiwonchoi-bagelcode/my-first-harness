fcf9dbd424207b4aae707e26b67267cfa843dfd4
Choe Chiwon <choechiwon@chiwonchoi-macos.local>
2026-09-03 17:05:40 +0900

    3. 시스템 프롬프트 넣기(현재 단일 스텝이라 멀티 스텝이 논리적으론 다음 단계이나 그게 가능하려면 LLM스스로 멈추는걸 해야되기 때문에 시스템 프롬프트 먼저 구현)

--- my-first-harness.ts
+++ my-first-harness.ts
@@ -1,42 +1,46 @@
-import 'dotenv/config'
-import { createInterface } from 'node:readline/promises'
+import "dotenv/config";
+import { createInterface } from "node:readline/promises";
 
-const token = process.env.AIPROXY_TOKEN
+const token = process.env.AIPROXY_TOKEN;
 
-const input = process.argv[2]
+const input = process.argv[2];
 
 const terminal = createInterface({
   input: process.stdin,
   output: process.stdout,
-})
-
+});
 
 async function step(input: string) {
   const response = await fetch(
-    'https://aiproxy-api.backoffice.bagelgames.com/api/v1/chat/openai',
+    "https://aiproxy-api.backoffice.bagelgames.com/api/v1/chat/openai",
     {
-      method: 'POST',
+      method: "POST",
       headers: {
         Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        messages: [{ role: 'user', content: input }],
+        messages: [
+          {
+            role: "system",
+            content: 'you are a mocking bird. just say "what?". **NO MATTER what user said.**',
+          },
+          { role: "user", content: input },
+        ],
       }),
     },
-  )
+  );
 
   const result = await response.json();
-  return result.content
+  return result.content;
 }
 
-while(true) {
-  const input = await terminal.question('> ')
+while (true) {
+  const input = await terminal.question("> ");
 
-  const output = await step(input)
+  const output = await step(input);
   // console.log(result.content)
   // console.log(response)
 
-
-  console.log(output)
+  console.log(output);
 }
